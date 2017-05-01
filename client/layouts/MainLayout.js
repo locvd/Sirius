903cc14015ae6f70e169c982e13de834970d4834
locvd <locvd89@gmail.com>
2017-05-01 21:25:35 +0900

    Check-in sample navbar

---
+++ client/layouts/MainLayout.js
@@ -0,0 +1,9 @@
+/**
+ * Created by locvd on 2017/05/01.
+ */
+
+import Initialize from '../imports/js/plugins';
+
+Template.MainLayout.onRendered(function () {
+  Initialize();
+});
--- client/patials/NavBar.js
+++ client/patials/NavBar.js
@@ -6,12 +6,5 @@ Template.navBar.onRendered(function () {
 });
 
 Template.navBar.events({
-  'click #nav-mobile'(event, instance){
-    let activeElement = jQuery('li.tab.active')[0];
-    let clickedElement = event.target.parentElement;
-    if (activeElement !== clickedElement) {
-      clickedElement.setAttribute('class', 'tab active');
-      activeElement.setAttribute('class', 'tab');
-    }
-  }
+
 });
