28270597dceb513f5273f4a900f45efc7a2d14dc
locvd <locvd89@gmail.com>
2017-05-04 23:31:13 +0900

    Upload multiple files

--- client/patials/SampleUpload.js
+++ client/patials/SampleUpload.js
@@ -2,7 +2,7 @@
  * Created by locvd on 2017/05/04.
  */
 
-import { ReactiveVar } from 'meteor/reactive-var';
+import {ReactiveVar} from 'meteor/reactive-var';
 
 Template.uploadedFiles.helpers({
   uploadedFiles: function () {
@@ -11,12 +11,16 @@ Template.uploadedFiles.helpers({
 });
 
 Template.uploadForm.onCreated(function () {
-  this.currentUpload = new ReactiveVar(false);
+  this.filesToUpload = new ReactiveVar([]);
+  this.uploads = {};
 });
 
 Template.uploadForm.helpers({
-  currentUpload: function () {
-    return Template.instance().currentUpload.get();
+  uploadInstance: function (file) {
+    return Template.instance().uploads[file.name].get();
+  },
+  filesToUpload: function () {
+    return Template.instance().filesToUpload.get();
   }
 });
 
@@ -25,29 +29,42 @@ Template.uploadForm.events({
     if (e.currentTarget.files && e.currentTarget.files[0]) {
       // We upload only one file, in case
       // there was multiple files selected
-      var file = e.currentTarget.files[0];
-      if (file) {
-        var uploadInstance = Images.insert({
-          file: file,
-          streams: 'dynamic',
-          chunkSize: 'dynamic'
-        }, false);
-
-        uploadInstance.on('start', function() {
-          template.currentUpload.set(this);
-        });
-
-        uploadInstance.on('end', function(error, fileObj) {
-          if (error) {
-            alert('Error during upload: ' + error.reason);
-          } else {
-            alert('File "' + fileObj.name + '" successfully uploaded');
-          }
-          template.currentUpload.set(false);
-        });
-
-        uploadInstance.start();
+      let fileList = e.currentTarget.files;
+      let files = [];
+      let uploads = {};
+      for (let i = 0; i < fileList.length; i++) {
+        let file = fileList[i];
+        files.push(file);
+        uploads[file.name] = new ReactiveVar(false);
       }
+
+      template.uploads = uploads;
+      template.filesToUpload.set(files);
+
+      files.forEach(function (file) {
+        if (file) {
+          let uploadInstance = Images.insert({
+            file: file,
+            streams: 'dynamic',
+            chunkSize: 'dynamic'
+          }, false);
+
+          uploadInstance.on('start', function () {
+            template.uploads[file.name].set(this);
+          });
+
+          uploadInstance.on('end', function (error, fileObj) {
+            if (error) {
+              console.log("error uploading");
+            } else {
+              // console.log(fileObj);
+            }
+            template.uploads[file.name].set(false);
+          });
+
+          uploadInstance.start();
+        }
+      });
     }
   }
 });
